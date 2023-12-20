4dd399c69a3e216c2e283e0d676e38170a312600
KHashimoto3 <hashi.koki385@gmail.com>
2023-12-20 14:45:31 +0900

    formのコントローラーにhello関数を作成した。

--- formcoder-api/src/form/form.controller.ts
+++ formcoder-api/src/form/form.controller.ts
@@ -1,4 +1,13 @@
+import { HttpService } from '@nestjs/axios';
 import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 
 @Controller('form')
-export class FormController {}
+export class FormController {
+  constructor(private readonly httpService: HttpService) {}
+
+  @Get('hello')
+  getHello(): { message: string } {
+    return { message: 'Hello World!' };
+  }
+}
--- formcoder-api/src/form/form.service.ts
+++ formcoder-api/src/form/form.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FormService {}
+export class FormService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
