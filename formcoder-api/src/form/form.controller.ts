1fb1982fa590c9efa91ad82097051ce55e578c43
KHashimoto3 <hashi.koki385@gmail.com>
2023-12-22 14:30:11 +0900

    formServiceを経由してhello worldを出力するようにした。

--- formcoder-api/src/form/form.controller.ts
+++ formcoder-api/src/form/form.controller.ts
@@ -1,13 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Controller } from '@nestjs/common';
 import { Get } from '@nestjs/common';
+import { FormService } from './form.service';
 
 @Controller('form')
 export class FormController {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly formService: FormService,
+  ) {}
 
   @Get('hello')
   getHello(): { message: string } {
-    return { message: 'Hello World!' };
+    return this.formService.getHello();
   }
 }
