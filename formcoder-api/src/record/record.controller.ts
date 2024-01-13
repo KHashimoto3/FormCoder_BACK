63396cdce447bafa17dbbdccf3489da5e3486883
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-13 15:59:39 +0900

    recordサービスに仮のhello関数を作った。

--- formcoder-api/src/record/record.controller.ts
+++ formcoder-api/src/record/record.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get } from '@nestjs/common';
+import { RecordService } from './record.service';
 
 @Controller('record')
-export class RecordController {}
+export class RecordController {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly recordService: RecordService,
+  ) {}
+
+  @Get('/hello')
+  getHello(): { message: string } {
+    return this.recordService.hello();
+  }
+}
--- formcoder-api/src/record/record.service.ts
+++ formcoder-api/src/record/record.service.ts
@@ -1,4 +1,26 @@
+import * as dotenv from 'dotenv';
 import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
+
+dotenv.config();
 
 @Injectable()
-export class RecordService {}
+export class RecordService {
+  private storage: Storage;
+  constructor() {
+    this.storage = new Storage({
+      projectId: process.env.PROJECT_ID,
+      credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      },
+    });
+  }
+
+  private backetName = process.env.BUCKET_NAME;
+
+  //テスト用のhello関数
+  hello(): { message: string } {
+    return { message: 'Hello World!' };
+  }
+}
