5f0bf3b6a83588e60a4e28f38901aca26aef0a2c
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-13 18:09:00 +0900

    recordInputDtoにバリデーションをつけた。

--- formcoder-api/src/dto/recordInput.dto.ts
+++ formcoder-api/src/dto/recordInput.dto.ts
@@ -1,12 +1,21 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class RecordInputDto {
+  @IsNotEmpty()
   readonly userId: string;
+
+  @IsNotEmpty()
   readonly formName: string;
+
+  @IsNotEmpty()
   readonly fbData: {
     id: number;
     partType: string;
     currentHintStep: number;
     clickedHintStep: number;
   }[];
+
+  @IsNotEmpty()
   readonly input: {
     id: number;
     partType: string;
