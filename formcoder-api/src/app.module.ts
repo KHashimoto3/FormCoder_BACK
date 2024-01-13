379d100454484e16cf0f8f30763b5b8e57c7011f
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-13 15:47:47 +0900

    record用のコントローラーとサービスファイルを作った。

--- formcoder-api/src/app.module.ts
+++ formcoder-api/src/app.module.ts
@@ -6,10 +6,12 @@ import { HttpModule } from '@nestjs/axios';
 import { FormService } from './form/form.service';
 import { HintController } from './hint/hint.controller';
 import { HintService } from './hint/hint.service';
+import { RecordController } from './record/record.controller';
+import { RecordService } from './record/record.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, FormController, HintController],
-  providers: [AppService, FormService, HintService],
+  controllers: [AppController, FormController, HintController, RecordController],
+  providers: [AppService, FormService, HintService, RecordService],
 })
 export class AppModule {}
