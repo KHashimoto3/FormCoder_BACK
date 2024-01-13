23e8433c65269a5ca7ac0b63e266499131ea641f
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-13 16:53:57 +0900

    解答用のテンプレートを取得するapiを作成した。

--- formcoder-api/src/record/record.controller.ts
+++ formcoder-api/src/record/record.controller.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { RecordService } from './record.service';
+import { TmpData } from 'src/type/tmpData';
 
 @Controller('record')
 export class RecordController {
@@ -12,5 +13,12 @@ export class RecordController {
   @Get('/hello')
   getHello(): { message: string } {
     return this.recordService.hello();
+  }
+
+  @Get('/tmp')
+  pullAnswerTemplate(
+    @Query('formName') formName: string,
+  ): Promise<{ tmpData: TmpData[] }> {
+    return this.recordService.pullAnswerTemplate(formName);
   }
 }
--- formcoder-api/src/record/record.service.ts
+++ formcoder-api/src/record/record.service.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { Storage } from '@google-cloud/storage';
+import { TmpData } from 'src/type/tmpData';
 
 dotenv.config();
 
@@ -22,5 +23,41 @@ export class RecordService {
   //テスト用のhello関数
   hello(): { message: string } {
     return { message: 'Hello World!' };
+  }
+
+  //cloud storageから、解答用のテンプレートをpullする
+  pullAnswerTemplate(formName: string): Promise<{ tmpData: TmpData[] }> {
+    if (formName === undefined) {
+      const errMessage = 'パラメータformNameは必須です。';
+      throw new HttpException(errMessage, 400);
+    }
+    try {
+      const bucket = this.storage.bucket(this.backetName);
+      const file = bucket.file('form/' + formName + '_tmp.json');
+      return new Promise<{ tmpData: TmpData[] }>((resolve, reject) => {
+        file.download((err, contents) => {
+          if (err) {
+            //ファイルが見つからなかった場合
+            if (err.message.includes('No such object')) {
+              const errMessage =
+                '指定されたフォーム名の回答テンプレートが見つかりません。';
+              console.log(err.message);
+              reject(new HttpException(errMessage, 404));
+            }
+            const errMessage = 'プル時にエラーが発生しました！';
+            console.log(err.message);
+            reject(new HttpException(errMessage, 500));
+          } else {
+            const recievedData = JSON.parse(contents.toString());
+            const tmpData: TmpData[] = recievedData.tmpData;
+            resolve({ tmpData: tmpData });
+          }
+        });
+      });
+    } catch (error) {
+      const errMessage = '何らかのエラーが発生しました。';
+      console.log(error.message);
+      throw new HttpException(errMessage, 500);
+    }
   }
 }
