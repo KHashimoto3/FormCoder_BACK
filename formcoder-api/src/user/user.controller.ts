893c10bc38adc51eb67473db426f90fe745c0dc1
KHashimoto3 <hashi.koki385@gmail.com>
2024-02-22 12:26:33 +0900

    ユーザーを追加する/user/registerエンドポイントとその関数を作成した。

--- formcoder-api/src/user/user.controller.ts
+++ formcoder-api/src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly userService: UserService,
+  ) {}
+
+  //cloud firestoreにユーザー情報をpushする
+  @Post('/register')
+  registUser(): Promise<{ message: string }> {
+    return this.userService.registUser();
+  }
+}
--- formcoder-api/src/user/user.service.ts
+++ formcoder-api/src/user/user.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Firestore } from '@google-cloud/firestore';
+import { HttpException, Injectable } from '@nestjs/common';
+import { User } from 'src/type/user';
+import { createHash } from 'crypto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private firestore: Firestore;
+  constructor() {
+    this.firestore = new Firestore({
+      projectId: process.env.PROJECT_ID,
+      credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      },
+      databaseId: 'form-hint-db',
+    });
+  }
+
+  //ユーザー情報をcloud firestoreにpushする
+  registUser(): Promise<{ message: string }> {
+    const pass = 'test151';
+    const hash = createHash('sha256');
+    const createdAt = new Date().toISOString();
+    const updatedAt = new Date().toISOString();
+    const finalLoginAt = new Date().toISOString();
+    hash.update(pass);
+    try {
+      const data: User = {
+        userId: 'test',
+        name: 'test',
+        password: hash.digest('hex'),
+        icon: 'test',
+        email: 'test@sample.com',
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        finalLoginAt: finalLoginAt,
+        deleteFlag: false,
+      };
+
+      const userRef = this.firestore.collection('user').doc(data.userId);
+      return new Promise<{ message: string }>((resolve, reject) => {
+        userRef
+          .set(data)
+          .then(() => {
+            resolve({ message: 'ユーザー情報を登録しました。' });
+          })
+          .catch((error) => {
+            console.log(error.message);
+            reject(new HttpException('登録時にエラーが発生しました。', 500));
+          });
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new HttpException('何らかのエラーが発生しました。', 500);
+    }
+  }
+}
